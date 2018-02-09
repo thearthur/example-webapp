95cc9faa4b727d78136399b12f5904d1a0fb04a6
Arthur Ulfeldt <arthur@ulfeldt.com>
2018-02-09 12:27:12 -0800

    pass alb to deploy job

--- run-stack.sh
+++ run-stack.sh
@@ -9,4 +9,4 @@ aws cloudformation $ACTION \
     ParameterKey=DockerImage,ParameterValue=089778365617.dkr.ecr.us-east-1.amazonaws.com/example-webapp:$(git rev-parse HEAD) \
     ParameterKey=VPC,ParameterValue=vpc-affe74c9 \
     ParameterKey=Cluster,ParameterValue=default \
-    ParameterKey=Listener,ParameterValue=arn:aws:elasticloadbalancing:us-east-1:089778365617:listener/app/production-website/12bff5f70d55ebb6/be1bf6afb6c78713
+    ParameterKey=Listener,ParameterValue=$ALB_LISTENER_ARN
